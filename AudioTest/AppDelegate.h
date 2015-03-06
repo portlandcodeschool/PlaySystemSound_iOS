42f5e0b8f7b58edfc1c782dcab9d20f626e0e8be
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2015-03-05 17:37:31 -0800

    Initial Commit

---
+++ AudioTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioTest
+//
+//  Created by Erick Bennett on 3/5/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioTest
+//
+//  Created by Erick Bennett on 3/5/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
